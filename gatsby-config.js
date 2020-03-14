5a2b025d4575f441c216bc6ffa5f901e110fa660
vikram <vikram.aroskar@gmail.com>
2020-03-14 15:00:12 +0530

    working with sanity

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,23 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: '4qjv3ycn',
+        dataset: 'production',
+
+        // a token with read permissions is required
+        // if you have a private dataset
+        token: 'skPFWWnivIF4wQ26nwucmo8d83iUzRLnP85hBC3baf9WN2hctXVur1ukm2VHVWbpeOuN2dVt1mCt93PkE8x4DNtA4zD6jwk6hw8DscVL2kqLwcBpbRsyLItBUp1mIiAHNbpPk0SHjPoqO7QIqu2KOoOkgrzaFv1qCqnpbfwkmMiQxYVFKZ3V',
+
+        // If the Sanity GraphQL API was deployed using `--tag <name>`,
+        // use `graphqlTag` to specify the tag name. Defaults to `default`.
+        graphqlTag: 'default',
+        overlayDrafts: true,
+        watchMode: true
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,47 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+//import Image from "../components/image"
+import Image from "gatsby-image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+export const query = graphql`
+  {
+    allSanityProject {
+      edges {
+        node {
+          id
+          title
+          description
+          slug {
+            current
+          }
+          image {
+            asset {
+              fluid {
+                ...GatsbySanityImageFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>My project portfolio from Sanity</h1>
+    <ul style={{listStyle:'none', display:'flex', alignItems:'space-between', padding: 0}}>
+      {data.allSanityProject.edges.map(({ node: project }) => (
+      <li key={project.slug.current} style={{flex: '1 45%', flexWrap:'wrap', maxWidth:'45%', margin: '1rem'}}>
+        <h2 style={{fontSize:'24px'}}>{project.title}</h2>
+        <Image fluid={project.image.asset.fluid} alt={project.title} />
+        <p style={{margin: '1rem'}}> {project.description}</p>
+      </li>
+    ))}</ul>
   </Layout>
 )
 
